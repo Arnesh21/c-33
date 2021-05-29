aaf8ac912d9ea08c30c9b42059f931518d381026
Arnesh21 <77525936+Arnesh21@users.noreply.github.com>
2021-05-29 13:23:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies =  Matter.Bodies;
+const Constraint = Matter.Constraint;
+var bg;
+var ice=[];
+var snowFall=100;
+
+function preload(){
+  bg=loadImage("snow2.jpg");
+}
+
+function setup() {
+  createCanvas(1300,600);
+  
+  engine=Engine.create();
+  world= engine.world;
+
+if(frameCount % 275 === 0){
+  for(var i=0; i<snowFall; i++){
+  ice.push(new Snow(random(0,1350), random(0,50)));
+  }
+}
+
+
+}
+
+function draw() {
+  background(bg);  
+  Engine.update(engine);
+
+
+//add gravity
+
+  for(var i = 0;i < snowFall; i++){
+    ice[i].display();
+    ice[i].changePosition();
+    }    
+
+    drawSprites();
+
+}
